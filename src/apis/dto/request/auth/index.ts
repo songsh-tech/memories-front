2ca0b2f7a4a40a659ab43d753a0bc981f49eb222
songsh-tech <hunsiskj16@naver.com>
2025-03-17 17:39:23 +0900

    2025.03.17. 17:40

--- src/apis/dto/request/auth/index.ts
+++ src/apis/dto/request/auth/index.ts
@@ -1,9 +1,12 @@
-import SignUpRequestDto from './sign-up.request.dto';
-import SignInRequestDto from './sign-in.request.dto';
-import IdCheckRequestDto from './id-check.request.dto';
+import { JoinType } from 'src/types/aliases';
 
-export type {
-  SignUpRequestDto,
-  SignInRequestDto,
-  IdCheckRequestDto
-}
+// interface: sign up request body DTO //
+export default interface SignUpRequestDto {
+  userId: string;
+  userPassword: string;
+  name: string;
+  address: string;
+  detailAddress: string | null;
+  joinType: JoinType;
+  snsId?: string;
+}   
